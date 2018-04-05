45d4caa9707e7d6447c23bc4336bc6588bcd6aa1
Seungri Cho <pseudo.circuit@gmail.com>
2018-04-05 11:56:38 +0900

    Login 구현

---
+++ app/members/serializers.py
@@ -0,0 +1,34 @@
+from django.contrib.auth import get_user_model, authenticate
+from rest_framework import serializers
+
+User = get_user_model()
+
+__all__ = (
+    'UserSerializer',
+    'AccessTokenSerializer',
+)
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            'pk',
+            'username',
+        )
+
+
+class AccessTokenSerializer(serializers.Serializer):
+    access_token = serializers.CharField()
+
+    def validate(self, attrs):
+        access_token = attrs.get('access_token')
+        if access_token:
+            user = authenticate(access_token=access_token)
+            if not user:
+                raise serializers.ValidationError('엑세스 토큰이 올바르지 않습니다.')
+        else:
+            raise serializers.ValidationError('엑세스 토큰이 필요합니다')
+
+        attrs['user'] = user
+        return attrs
