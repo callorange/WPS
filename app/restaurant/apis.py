0cac50bee0610b03f393f7d275c09f59a03e1d0b
jeongyeob <sjy20000@gmail.com>
2018-04-09 05:23:54 +0900

    식당 카테고리 목록 api 추가

--- app/restaurant/apis.py
+++ app/restaurant/apis.py
@@ -1,5 +1,18 @@
+from rest_framework import status
+from rest_framework.generics import ListAPIView
+from rest_framework.response import Response
 from rest_framework.views import APIView
+
+from .serializers import FoodCategorySerializer
+from .models import FoodCategory
 
 
 class Restaurant(APIView):
     pass
+
+
+class FoodCategoryView(APIView):
+
+    def get(self, request, format=None):
+        serializer = FoodCategorySerializer(FoodCategory.objects.all(), many=True)
+        return Response({'categories': serializer.data}, status=status.HTTP_200_OK)
--- app/restaurant/urls.py
+++ app/restaurant/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .apis import Restaurant
+from .apis import Restaurant, FoodCategoryView
 
 app_name = 'restaurant'
 
 urlpatterns = [
-    path('', Restaurant.as_view(), name="list")
+    path('', Restaurant.as_view(), name="list"),
+    path('category/', FoodCategoryView.as_view(), name="category-list")
 ]
