5e76f92b1e494f04ec57adb6037d8660a9fcaa3a
Seungri Cho <pseudo.circuit@gmail.com>
2018-04-12 16:00:08 +0900

    default image 테스트 중

--- app/members/serializers.py
+++ app/members/serializers.py
@@ -18,7 +18,7 @@ class UserSerializer(serializers.ModelSerializer):
         ],
     )
     password = serializers.CharField(min_length=5, write_only=True)
-    img_profile = serializers.ImageField(default='')
+    img_profile = serializers.ImageField(default='img_profile/profile_dummy.png')
 
     def create(self, validated_data):
         print(validated_data)
