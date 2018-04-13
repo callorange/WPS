e57abe64dc7888fb3fe3eb57e9b813ef0b34c49f
Seungri Cho <pseudo.circuit@gmail.com>
2018-04-13 17:56:34 +0900

    user detail view 완성

--- app/members/apis.py
+++ app/members/apis.py
@@ -2,6 +2,8 @@ from django.contrib.auth import get_user_model
 from rest_framework import status, permissions, generics
 from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.generics import get_object_or_404
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import UserSerializer
@@ -12,6 +14,31 @@ User = get_user_model()
 class UserListView(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class UserDetail(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self, pk, format=None):
+        return get_object_or_404(User, pk=pk)
+
+    def get(self, request, pk, format=None):
+        user = self.get_object(pk=pk)
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        user = self.get_object(pk=pk)
+        serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        user = self.get_object(pk=pk)
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class UserCreate(APIView):
