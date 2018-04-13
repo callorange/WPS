d216908391ee7caa7cd1022b45ccfba0276fe291
Seungri Cho <pseudo.circuit@gmail.com>
2018-04-13 17:56:34 +0900

    user detail view 완성

--- app/members/urls.py
+++ app/members/urls.py
@@ -2,10 +2,11 @@
 
 from django.urls import path
 
-from .apis import UserCreate, UserListView
+from .apis import UserCreate, UserListView, UserDetail
 
 urlpatterns = [
     path('', UserListView.as_view()),
     path('user/', UserCreate.as_view()),
+    path('user/<int:pk>', UserDetail.as_view()),
 
 ]
